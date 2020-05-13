0194c9826f9c8af68f2cd74a1c1774f75d466b57
Julius <Julius>
2020-05-13 17:25:51 +0300

    Loop and arr

--- src/Car_Races.java
+++ src/Car_Races.java
@@ -1,11 +1,26 @@
 /**
  *
- * @author evuncik
+ * @author Julius
  */
 public class Car_Races {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        
+        int[] m = {1, 3, 5, 2, -2, -1, 10};
+        
+            for(int i = 0; i < m.length - 1; i++) {
+                for(int x = i + 1; x < m.length; x++) {
+                   if(m[i] < m[x]) {
+                       int temp = m[i];
+                       m[i] = m[x];
+                       m[x] = temp;
+                   } 
+                }
+            }
+            for(int i = 0; i < m.length; i++) {
+                System.out.println(m[i]);
+            }
+        
     }
     
 }
